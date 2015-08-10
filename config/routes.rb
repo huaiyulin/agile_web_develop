18872c0da8bc16bb4ae8dcf7e4405201744e5df0
sirius733 <sirius733@gmail.com>
2015-08-10 16:49:33 +0800

    fundamental finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'store/index'
+  root 'store#index'
 
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
